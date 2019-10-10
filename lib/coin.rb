a0697a805dad5ddcf86469ab404af6ca827108ea
Cédric RENAUD <ruthhaba@gmail.com>
2019-10-11 00:20:35 +0200

    update

--- lib/coin.rb
+++ lib/coin.rb
@@ -2,15 +2,13 @@ require 'nokogiri'
 require 'open-uri'
 
 
-def scrap_nom
+
 nom_crypto=[]
 page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
 page.xpath('//td[2]/a').each do |nom|
 nom_crypto << nom.text
 end
 puts nom_crypto
-
-
 
 value_crypto=[]
 page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
